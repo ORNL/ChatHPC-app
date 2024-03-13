d0d89c82e3ef1208294f472271ed8be9fdbe63f2
Aaron Young <youngar@ornl.gov>
2024-03-13 17:17:14 -0400

    Remove MIT License.

--- src/python_project_template/__about__.py
+++ src/python_project_template/__about__.py
@@ -1,4 +1,1 @@
-# SPDX-FileCopyrightText: 2024-present Aaron Young <youngar@ornl.gov>
-#
-# SPDX-License-Identifier: MIT
 __version__ = "0.0.1"
--- src/python_project_template/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present Aaron Young <youngar@ornl.gov>
-#
-# SPDX-License-Identifier: MIT
