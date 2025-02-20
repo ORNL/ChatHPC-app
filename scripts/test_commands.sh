0c0278604294b278a8c2484792b5c7ba4faa2c24
Aaron Young <youngar@ornl.gov>
2025-02-20 11:17:19 -0500

    Update test_commands script.

--- scripts/test_commands.sh
+++ scripts/test_commands.sh
@@ -18,13 +18,11 @@ echo CHATHPC_DATA_FILE="/home/7ry/Data/ellora/kokkos-data/kokkos_create_context.
     CHATHPC_FINETUNED_MODEL_PATH="./app/peft_adapter"\
     CHATHPC_MERGED_MODEL_PATH="./app/merged_adapters"\
     CHATHPC_TRAINING_OUTPUT_DIR="./app/kokkos-code-llama"\
-    CHATHPC_TRAINING_PROMPT="You are a powerful LLM model for Kokkos. Your job is to answer questions about Kokkos programming model. You are given a question and context regarding Kokkos programming model.\n\nYou must output the Kokkos question that answers the question.\n\n### Input:\n{question}\n\n### Context:\n{context}\n\n### Response:\n{answer}\n"\
-    CHATHPC_INFERENCE_PROMPT="You are a powerful LLM model for Kokkos. Your job is to answer questions about Kokkos programming model. You are given a question and context regarding Kokkos programming model.\n\nYou must output the Kokkos question that answers the question.\n\n### Input:\n{question}\n\n### Context:\n{context}\n\n### Response:\n"\
+    CHATHPC_PROMPT_TEMPLATE="You are a powerful LLM model for Kokkos called ChatKokkos created by ORNL. Your job is to answer questions about the Kokkos programming model. You are given a question and context regarding the Kokkos programming model.\n\nYou must output the answer the question.\n\n### Context:\n{{ context }}\n\n### Question:\n{{ question }}\n\n### Answer:\n{{ answer }}\n\n"\
     chathpc config
 CHATHPC_DATA_FILE="/home/7ry/Data/ellora/kokkos-data/kokkos_create_context.json"\
     CHATHPC_FINETUNED_MODEL_PATH="./app/peft_adapter"\
     CHATHPC_MERGED_MODEL_PATH="./app/merged_adapters"\
     CHATHPC_TRAINING_OUTPUT_DIR="./app/kokkos-code-llama"\
-    CHATHPC_TRAINING_PROMPT="You are a powerful LLM model for Kokkos. Your job is to answer questions about Kokkos programming model. You are given a question and context regarding Kokkos programming model.\n\nYou must output the Kokkos question that answers the question.\n\n### Input:\n{question}\n\n### Context:\n{context}\n\n### Response:\n{answer}\n"\
-    CHATHPC_INFERENCE_PROMPT="You are a powerful LLM model for Kokkos. Your job is to answer questions about Kokkos programming model. You are given a question and context regarding Kokkos programming model.\n\nYou must output the Kokkos question that answers the question.\n\n### Input:\n{question}\n\n### Context:\n{context}\n\n### Response:\n"\
+    CHATHPC_PROMPT_TEMPLATE="You are a powerful LLM model for Kokkos called ChatKokkos created by ORNL. Your job is to answer questions about the Kokkos programming model. You are given a question and context regarding the Kokkos programming model.\n\nYou must output the answer the question.\n\n### Context:\n{{ context }}\n\n### Question:\n{{ question }}\n\n### Answer:\n{{ answer }}\n\n"\
     chathpc config
